785f9f77fd123467ef0b89e3534fa8b64b775e1f
Nikolay Ushmodin <ushmodin@gmail.com>
2018-02-07 16:17:17 +0700

    jwtautorization

--- game/server.go
+++ game/server.go
@@ -5,10 +5,14 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/auth0/go-jwt-middleware"
 	jwt "github.com/dgrijalva/jwt-go"
 
 	"github.com/gorilla/mux"
 )
+
+var jwtKey = []byte("12345678")
+var jwtSignMethod = jwt.SigningMethodHS256
 
 type CrisCrossServer struct {
 	game *CrisCrossGame
@@ -24,10 +28,22 @@ func (srv *CrisCrossServer) ListenAndServe(addr string) {
 }
 
 func (srv *CrisCrossServer) createRouter() *mux.Router {
+
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		},
+		SigningMethod: jwtSignMethod,
+		ErrorHandler: func(w http.ResponseWriter, r *http.Request, err string) {
+			writeError(w, NewGameError(AUTH_ERROR, err))
+		},
+	})
+
 	r := mux.NewRouter()
 	r.HandleFunc("/api/ping", pingHandler).Methods("GET")
 	r.HandleFunc("/api/reg", srv.regHandler).Methods("POST")
 	r.HandleFunc("/api/auth", srv.authHandler).Methods("POST")
+	r.Handle("/api/game/start", jwtMiddleware.Handler(http.HandlerFunc(srv.startGame))).Methods("GET")
 	return r
 }
 
@@ -79,6 +95,10 @@ func (srv *CrisCrossServer) authHandler(w http.ResponseWriter, r *http.Request) 
 	}
 }
 
+func (srv *CrisCrossServer) startGame(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
 func writeError(w http.ResponseWriter, err error) {
 	gameError, ok := err.(gameError)
 	code := UNKNOW_ERROR
@@ -94,11 +114,11 @@ func writeError(w http.ResponseWriter, err error) {
 }
 
 func createToken(username string) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+	token := jwt.NewWithClaims(jwtSignMethod, jwt.StandardClaims{
 		ExpiresAt: time.Now().Add(time.Hour * time.Duration(1)).UnixNano(),
 		Issuer:    username,
 	})
-	tokenStr, err := token.SignedString([]byte("12345678"))
+	tokenStr, err := token.SignedString(jwtKey)
 	if err != nil {
 		return "", err
 	}
